6afa40df91c8097363d7c32c72554cd57eafbfb2
qaz create <codewarcode@protonmail.com>
2021-02-14 16:10:50 -0500

    disabled submit button

--- js/main.js
+++ js/main.js
@@ -167,9 +167,39 @@
   });
   //portfolio end
 
+  // form submit button
+  $(document).ready(function() {
+    validate();
+    $('input').on('keyup', validate);
+  });
+  
+  function validate() {
+    var inputsWithValues = 0;
+    
+    // get all input fields except for type='submit'
+    var myInputs = $("input:not([type='submit'])");
+  
+    myInputs.each(function(e) {
+      // if it has a value, increment the counter
+      if ($(this).val()) {
+        inputsWithValues += 1;
+      }
+    });
+  
+    if (inputsWithValues == myInputs.length) {
+      $("input[type=submit]").prop("disabled", false);
+    } else {
+      $("input[type=submit]").prop("disabled", true);
+    }
+  }
+
+
+
+  // end form submit button
+
 })(jQuery);
 
 
 /*---------------------
-     YELLOW > IMAGE
+     
 ---------------------*/
